2b53cee6cc782e7623594d31ef64509ea18b536f
cantonice <250783329@qq.com>
2025-03-20 17:54:12 +0800

    conda安装脚本、github ssh连接配置脚本

---
+++ github_ssh.sh
@@ -0,0 +1,18 @@
+# 配置github的ssh（可选）
+cd $HOME/.ssh && touch config
+cat <<EOT >> config
+Host github.com
+    HostName ssh.github.com
+    Port 443
+    User git
+    PreferredAuthentications publickey
+    IdentityFile ~/.ssh/id_rsa
+EOT
+
+echo "安装git-lfs......."
+conda install -c conda-forge git-lfs
+
+echo "配置github的ssh......."
+ssh-keygen -t rsa -b 4096 -C "your email"
+cat .ssh/id_rsa.pub
+echo "---请将上面的公钥添加到github的ssh key中---"
---
+++ install_anaconda.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 定义Anaconda安装包URL和文件名
+ANACONDA_URL="https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh"
+INSTALLER="Anaconda3-2024.10-1-Linux-x86_64.sh"
+
+# 检查安装包是否已存在
+if [ ! -f "$INSTALLER" ]; then
+    echo "下载Anaconda安装包..."
+    wget $ANACONDA_URL
+else
+    echo "安装包已存在，跳过下载步骤"
+fi
+
+bash $INSTALLER -p $HOME/anaconda3
+
+# 添加环境变量
+$HOME/anaconda3/bin/conda init bash && source .bashrc
+
+echo "Anaconda安装完成！"
+
+# 配置清华镜像
+cd $HOME/anaconda3
+cat <<EOT >> ~/.condarc
+channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/msys2/
+show_channel_urls: true
+ 
+ssl_verify: true
+allow_conda_downgrades: true
+EOT
+ 
